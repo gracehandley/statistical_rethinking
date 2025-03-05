284db181ff226f77ad424aa62bf62b300b4b6cc0
Grace Handley <gracehandley@gmail.com>
2025-03-05 17:15:57 -0500

    Adding chapter_01.R to Git

---
+++ chapters/chapter_01.R
@@ -0,0 +1,8 @@
+# Statistical Rethinking 2nd edition by Richard McElreath
+# Chapter 2: R exercises
+# Author: Grace Handley
+# Date created: 2025-03-03
+# Date updated: 2025-03-03
+
+ways <- c(0, 3, 8, 9, 0)
+ways/sum(ways)
